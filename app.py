072dd1ea69b792abcfe5e0a4248c30cfacfd76aa
Luchotheboss <143089265+Luchotheboss@users.noreply.github.com>
2023-08-24 07:11:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 # tata
 import streamlit as st
+from PIL import image
+
 st.title("Mi primera app")
+
+st.header("Aplicaciones")
+st.write("Backend y frontend")
+image = Image.open('png-transparent-cat-kitty-creative-cat-cat')
+
+st.image(image, caption= 'Interfaces multimodales')
