a7248895392c2c6e1dbb3b87e30fed5b0af29ec3
Luchotheboss <143089265+Luchotheboss@users.noreply.github.com>
2023-08-24 07:28:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,14 @@ st.write("Backend y frontend")
 image = Image.open('gatico.png')
 
 st.image(image, caption= 'Interfaces multimodales')
+
+st.header("2 columnas")
+
+col1, col2 = st.colums(2)
+
+with col1:
+  st.subheader("Primera columna")
+  st.write("las interfaces multimodales mejoran la experiencia")
+  resp = st,checkbox('estoy de acuerdo')
+  if resp:
+    st.write('Correctol')
